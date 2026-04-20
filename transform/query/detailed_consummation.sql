aab90d525419dcf457d97db1e70d709d99bf836a
Emii-lia <fiaromiangaly@gmail.com>
2026-04-20 20:38:53 +0300

    fix(transform): fixing consommation transform error

--- transform/query/detailed_consummation.sql
+++ transform/query/detailed_consummation.sql
@@ -1,5 +1,6 @@
-SELECT ct.id_consommation, d.mois, d.annee, c.carburant_type, p.nom
+SELECT ct.id_consommation, d.mois, d.annee, c.carburant_type, p.nom, ct.quantite
   FROM analytics.consommation as ct
   JOIN analytics.date as d ON d.id_date = ct.id_date
   JOIN analytics.carburant as c ON c.id_carburant = ct.id_carburant
-  JOIN analytics.province p ON p.id_province = ct.id_province;
+  JOIN analytics.province p ON p.id_province = ct.id_province
+  ORDER BY p.nom, d.id_date, c.carburant_type;
