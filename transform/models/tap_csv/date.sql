e1ec284e8cb385874ad5fc54a082f623fbc23e48
Emii-lia <fiaromiangaly@gmail.com>
2026-04-20 11:19:33 +0300

    refactor(transform): updating date month and year column label for date and consommation entity

--- transform/models/tap_csv/date.sql
+++ transform/models/tap_csv/date.sql
@@ -9,4 +9,4 @@ with antananarivo as (
   select * from tap_csv.antananarivo
 )
 
-SELECT row_number() OVER () AS id_date, EXTRACT(MONTH FROM to_date("Mois", 'DD/MM/YY')) AS month, EXTRACT(YEAR FROM to_date("Mois", 'DD/MM/YY')) AS year FROM antananarivo
+SELECT row_number() OVER () AS id_date, EXTRACT(MONTH FROM to_date("Mois", 'DD/MM/YY')) AS mois, EXTRACT(YEAR FROM to_date("Mois", 'DD/MM/YY')) AS annee FROM antananarivo
