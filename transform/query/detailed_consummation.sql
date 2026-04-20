71851a024d939c74a92a8b8d93e0eee1da33a5b5
Emii-lia <fiaromiangaly@gmail.com>
2026-04-20 11:19:49 +0300

    feat(transform): adding a detailed query for consumption data

---
+++ transform/query/detailed_consummation.sql
@@ -0,0 +1,5 @@
+SELECT ct.id_consommation, d.mois, d.annee, c.carburant_type, p.nom
+  FROM analytics.consommation as ct
+  JOIN analytics.date as d ON d.id_date = ct.id_date
+  JOIN analytics.carburant as c ON c.id_carburant = ct.id_carburant
+  JOIN analytics.province p ON p.id_province = ct.id_province;
